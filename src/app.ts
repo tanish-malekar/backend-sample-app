107b21a27c9d9bea6a4fe1314dac1f85b2184d24
roerohan <roerohan@gmail.com>
2021-11-23 20:07:22 +0530

    feat(cors): added cors for all domains

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import routes from './routes';
@@ -7,10 +8,11 @@ import logger from './util/logger';
 async function main() {
     dotenv.config();
     const app = express();
+    app.use(cors());
     app.use(express.json());
     app.use(morgan('dev'));
 
-    const { PORT } = process.env;
+    const { PORT = 3000 } = process.env;
 
     app.use(routes);
 
